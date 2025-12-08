fea4d13ed364e7b23f068ff2048c271e4e1f898b
levong23521803 <23521803@gm.uit.edu.vn>
2025-12-08 19:25:54 +0700

    code

---
+++ tetris.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define H 20
+#define W 15
+
+char board[H][W] = {} ;
+
+char blocks[][4][4] = {
+        // 0: I (dọc)
+        {{' ','I',' ',' '},
+         {' ','I',' ',' '},
+         {' ','I',' ',' '},
+         {' ','I',' ',' '}},
+        // 1: O (vuông)
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        // 2: T
+        {{' ',' ',' ',' '},
+         {' ','T',' ',' '},
+         {'T','T','T',' '},
+         {' ',' ',' ',' '}},
+        // 3: S
+        {{' ',' ',' ',' '},
+         {' ','S','S',' '},
+         {'S','S',' ',' '},
+         {' ',' ',' ',' '}},
+        // 4: Z
+        {{' ',' ',' ',' '},
+         {'Z','Z',' ',' '},
+         {' ','Z','Z',' '},
+         {' ',' ',' ',' '}},
+        // 5: J
+        {{' ',' ',' ',' '},
+         {'J',' ',' ',' '},
+         {'J','J','J',' '},
+         {' ',' ',' ',' '}},
+        // 6: L
+        {{' ',' ',' ',' '},
+         {' ',' ','L',' '},
+         {'L','L','L',' '},
+         {' ',' ',' ',' '}}
+};
+
+int x=4,y=0,b=1;
+
+void gotoxy(int x, int y) {
+    COORD c = {(SHORT)x, (SHORT)y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void boardDelBlock(){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' ' && y+j < H)
+                board[y+i][x+j] = ' ';
+}
+
+void block2Board(){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' ' )
+                board[y+i][x+j] = blocks[b][i][j];
+}
+
+void initBoard(){
+    for (int i = 0 ; i < H ; i++)
+        for (int j = 0 ; j < W ; j++)
+            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
+            else board[i][j] = ' ';
+}
+
+void draw(){
+    gotoxy(0,0);
+    for (int i = 0 ; i < H ; i++, cout<<endl)
+        for (int j = 0 ; j < W ; j++)
+            cout<<board[i][j];
+}
+
+bool canMove(int dx, int dy){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' '){
+                int tx = x + j + dx;
+                int ty = y + i + dy;
+                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
+                if ( board[ty][tx] != ' ') return false;
+            }
+    return true;
+}
+
+// SV2 sẽ viết hàm removeLine() ở đây
+// Hàm này sẽ xóa các dòng đã đầy và di chuyển các dòng phía trên xuống
+
+void rotate(){
+    // SV4 sẽ viết code xoay block ở đây
+}
+
+int main()
+{
+    srand(time(0));
+    b = rand() % 7;
+    system("cls");
+    initBoard();
+    
+    while (1){
+        boardDelBlock();
+        
+        // Khối rơi xuống tự động (yêu cầu SV1)
+        if (canMove(0,1)) y++;
+        else {
+            block2Board();
+            // SV2 sẽ gọi removeLine() ở đây sau khi đặt block
+            // removeLine();
+            x = 5; y = 0; b = rand() % 7;
+        }
+        
+        block2Board();
+        draw();
+        Sleep(200);
+    }
+    
+    return 0;
+}
+
